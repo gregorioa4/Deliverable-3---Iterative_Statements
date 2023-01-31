c3c75cea73e1ae83761c3a3a06c3598de0dc84c8
antsg <antsg@ANTHONYS-DESKTO>
2023-01-30 20:17:48 -0500

    Add project files.

---
+++ Deliverable (3)/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+Author: Anthony Gregorio
+Date: 1/23/2023
+Description: C# Console Application number 1-100
+*/
+
+
+using System;
+namespace Deliverable__3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Enter an integer number between 1 and 100");
+                input = int.Parse(Console.ReadLine());
+
+                if (input >= 1 && input <= 100)
+                {
+                    break;
+                }
+            }
+           
+
+            Console.Write("Specify the series type: Even or Odd -  ");
+            string series = Console.ReadLine();
+
+
+            if (series == "Odd")
+            {
+                Console.WriteLine("You have selected " + series + " series. The numbers between 0 and " + input + " are:");
+                for (int i = 1; i < input; i = i + 2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            if (series == "Even")
+            {
+                Console.WriteLine("You have selected " + series + " series. The numbers between 0 and " + input + " are:");
+                for (int i = 0; i < input; i = i + 2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
